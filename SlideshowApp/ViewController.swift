99b0c762495e3857020bacabf017c796cab38baa
赤トマト <redtomatogame@akatomatonoimac.local>
2018-03-17 13:17:18 +0900

    自動で3つの画像をスライドする機能実装 "onTimer"の部分は未修正

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  SlideshowApp
 //
-//  Created by 赤トマト on 2018/03/17.
+//  Created by 赤津亮太 on 2018/03/17.
 //  Copyright © 2018年 Ryota.Akatsu. All rights reserved.
 //
 
@@ -10,16 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    // 一定の間隔で処理を行うためのタイマーを作成
+    var timer: Timer?
+    
+    // 表示している画像の番号
+    var dispImageNo = 0
+    
+    // 表示している画像の番号を元に画像を表示する
+    func displayImage() {
+        
+        // 画像の名前の配列
+        let imageNameArray = [
+            "1",
+            "2",
+            "3",
+        ]
+        
+        // 画像の番号が正常な範囲を指しているかチェック
+        
+        // 範囲より下を指している場合、最後の画像を表示
+        if dispImageNo < 0 {
+            dispImageNo = 3
+        }
+        
+        // 範囲より上を指している場合、最初の画像を表示
+        if dispImageNo > 3 {
+            dispImageNo = 0
+        }
+        
+        // 表示している画像の番号から名前を取り出し
+        let name = imageNameArray[dispImageNo]
+        
+        // 画像を読み込み
+        let image = UIImage(named: name)
+        
+        // Image Viewに読み込んだ画像をセット
+        ImageView.image = image
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    
+        let image = UIImage(named: "1")
+        ImageView.image = image
+        
+        // タイマーを設定
+        timer = Timer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(ViewController.onTimer(_:)),userInfo: nil, repeats: true)
+      
     }
-
+    
+    // NSTimerによって一定の間隔で呼び出される関数
+    func onTimer(timer: Timer) {
+        
+        // 関数が呼ばれていることを確認
+        print("onTimer")
+        
+        // 表示している画像の番号を1増やす
+        dispImageNo += 1
+        
+        // 表示している画像の番号を元に画像を表示する
+        displayImage()
+    }
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
+    @IBAction func BackButton(_ sender: Any) {
+    }
+    
+    @IBAction func PlayButton(_ sender: Any) {
+    }
+    
+    @IBAction func NextButton(_ sender: Any) {
+    }
+    
+    
 }
-
